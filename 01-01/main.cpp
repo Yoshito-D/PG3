434ffa963e676ebbe4d5435deee3ab44c146c9d5
Yoshito17 <k024g0035@g.neec.ac.jp>
2025-12-13 11:57:10 +0900

    未使用のインクルードを修正。

--- 01-01/main.cpp
+++ 01-01/main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-#include <Windows.h>
 #include <list>
 #include <cstdint>
+#include <cstring>
 
 void PrintStationsWithInsert(std::list<const char*>& stations, int32_t year, const char* target = nullptr, const char* insertAfter = nullptr);
 
 int main() {
-	std::list<const char*> yamanoteStations{
-		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori", "Tabata",
-		"Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Shin-Okubo",
-		"Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki",
-		"Shinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
-	};
+   std::list<const char*> yamanoteStations{
+	   "Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani", "Nippori", "Tabata",
+	   "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Shin-Okubo",
+	   "Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki",
+	   "Shinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+   };
 
-	PrintStationsWithInsert(yamanoteStations, 1970, "Tabata", "Nishi-Nippori");
-	PrintStationsWithInsert(yamanoteStations, 2019, "Tamachi", "Takanawa Gateway");
-	PrintStationsWithInsert(yamanoteStations, 2022);
+   PrintStationsWithInsert(yamanoteStations, 1970, "Tabata", "Nishi-Nippori");
+   PrintStationsWithInsert(yamanoteStations, 2019, "Tamachi", "Takanawa Gateway");
+   PrintStationsWithInsert(yamanoteStations, 2022);
 
-	return 0;
+   return 0;
 }
 
 void PrintStationsWithInsert(std::list<const char*>& stations, int32_t year, const char* target, const char* insertAfter) {
-	std::cout << "\n" << year << std::endl;
+   std::cout << "\n" << year << std::endl;
 
-	for (auto itr = stations.begin(); itr != stations.end(); ++itr) {
-		std::cout << "- " << *itr << std::endl;
+   for (auto itr = stations.begin(); itr != stations.end(); ++itr) {
+	  std::cout << "- " << *itr << std::endl;
 
-		if (target && insertAfter && *itr == target) {
-			itr = stations.insert(itr, insertAfter);
-			++itr;
-		}
-	}
+	  if (target && insertAfter && std::strcmp(*itr, target) == 0) {
+		 itr = stations.insert(itr, insertAfter);
+		 ++itr;
+	  }
+   }
 }
